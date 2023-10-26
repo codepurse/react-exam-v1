12b33cb41ef6a27a15b3e2a1b6d0142cb4655c68
Alfon Labadan <alfonlabadan@gmail.com>
2023-10-27 00:07:25 +0800

    New exam

---
+++ pages/bomb.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+export default function Bomb() {
+  const [texts, setTexts] = useState([]);
+  const [arr, setArr] = useState([]);
+  const [reset, setReset] = useState(false);
+  const [inc, setInc] = useState(0);
+  const [num, setNum] = useState(12);
+
+  const generateRandomBox = () => {
+    const randomTexts = Array.from({ length: num }, () =>
+      Math.random() < 0.5 ? "Safe" : "Bomb"
+    );
+    setTexts(randomTexts);
+  };
+
+  const handleClick = (text, i) => {
+    if (reset) return;
+    setArr([...arr, i]);
+    setReset(text === "Bomb");
+  };
+
+  const handleReset = () => {
+    setInc(inc + 1);
+    setReset(false);
+    setArr([]);
+  };
+
+  useEffect(() => {
+    generateRandomBox();
+  }, [inc, num]);
+
+  const handleChange = (e) => setNum(e.currentTarget.value);
+
+  return (
+    <div className="h-100 align-items-center justify-content-center d-flex">
+      <div>
+        <input type="text" className="w-100 mb-2" onChange={handleChange} />
+        <div className="divParentColor">
+          {texts.map((text, i) => (
+            <div className="box" key={i} onClick={() => handleClick(text, i)}>
+              <p>{arr.includes(i) ? text : ""}</p>
+            </div>
+          ))}
+        </div>
+        {reset && (
+          <button className="mx-auto d-flex mt-2" onClick={handleReset}>
+            Reset
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
